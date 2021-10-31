b1da623a0885d2c9d3d07e5b46ed9ef60599b147
Diego Padilla <61509950+dieegopa@users.noreply.github.com>
2021-10-31 18:19:16 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   reactStrictMode: true,
   env: {
     backendURL: "https://grisly-goosebumps-74230.herokuapp.com",
-    frontendURL: "https://nodesend-nextjs-oovplhr6l-dieegopa.vercel.app",
+    frontendURL: "https://nodesend-cliente-nextjs.netlify.app",
   },
 };
